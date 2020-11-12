b8d97a2120505bc45549d95e19f754b850a8aabb
eltirder0 <eltirder0@gmail.com>
2020-11-12 20:52:59 +0700

    update css and app.js, adding folder auth and home, confirm_pass_customer, confirm_pass_seller, reset_pass_customer, reset_pass_seller, sign_up. Done auth part

---
+++ app.js
@@ -0,0 +1,16 @@
+const activeButton = (button) => {
+	const contentPartId = button.dataset.id
+	const buttons = document.querySelectorAll(".cs-btn-type")
+	const contentPartSection = document.querySelectorAll(".content-part")
+	console.log(contentPartId)
+	// remove class active-type and show-section
+	buttons.forEach((item) => item.classList.remove("active-type"))
+	contentPartSection.forEach((item) => item.classList.remove("show-section"))
+	// add class active-type and show-section
+	button.classList.add("active-type")
+	document.querySelector(`#${contentPartId}`).classList.add("show-section")
+}
+
+const redirectTo = (url) => {
+	window.location.href = url
+}
